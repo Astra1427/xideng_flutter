3fd92a7b2ae72515c6c8017ae67f51166b784b47
Ling <1427917847@qq.com>
2023-03-05 23:01:34 +0800

    配置 Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:xideng_flutter/main_navigator_page.dart';
 import 'package:xideng_flutter/pages/home_page.dart';
 import 'package:xideng_flutter/providers/account_provider.dart';
+import 'package:xideng_flutter/providers/app_config_provider.dart';
+import 'package:xideng_flutter/providers/skills_provider.dart';
 import 'package:xideng_flutter/providers/theme_provider.dart';
 import 'package:xideng_flutter/styles/main_style.dart';
 
@@ -18,10 +21,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AppThemeProvider()),
-        ChangeNotifierProvider(create: (context)=> AccountProvider())
+        ChangeNotifierProvider(create: (context)=> AccountProvider()),
+        ChangeNotifierProvider(create: (context)=> AppConfigProvider()),
+        ChangeNotifierProvider(create: (context)=> SkillsProvider())
       ],
       child: Consumer<AppThemeProvider>(
         builder: (context, provider, child) {
